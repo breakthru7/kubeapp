69d8ad4cfcf25bf42e04264831c4242759250e47
Eric Chong <eric.chong@cheetahdigital.com>
2022-01-06 10:55:28 +0800

    first

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace kubeApp.Pages
+{
+	public class IndexModel : PageModel
+	{
+		private readonly ILogger<IndexModel> _logger;
+
+		protected readonly IConfiguration _configuration;
+
+
+		public IndexModel(ILogger<IndexModel> logger, IConfiguration configuration)
+		{
+			_logger = logger;
+			_configuration = configuration;
+		}
+
+		public void OnGet()
+		{
+			ViewData["test"] = Content(_configuration.GetValue<string>("ValuesConfiguration:Message"), "text/plain").Content.ToString();
+		}
+	}
+
+}
